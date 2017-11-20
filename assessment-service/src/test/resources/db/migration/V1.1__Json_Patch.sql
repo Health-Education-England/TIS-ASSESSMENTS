a8625b3b81f880d91f319516b83fab548f1a534a
paulhoang-hee <paul.hoang@hee.nhs.uk>
2017-11-20 09:32:07 +0000

    fix fly migration scripts

--- assessment-service/src/test/resources/db/migration/V1.1__Json_Patch.sql
+++ assessment-service/src/test/resources/db/migration/V1.1__Json_Patch.sql
@@ -2,7 +2,7 @@ CREATE TABLE `JsonPatch` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `tableDtoName` varchar(255) NOT NULL,
   `patchId` varchar(255) DEFAULT NULL,
-  `patch` json DEFAULT NULL,
+  `patch` varchar(255) DEFAULT NULL,
   `dateAdded` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `enabled` bit(1) DEFAULT '1',
   PRIMARY KEY (`id`)
