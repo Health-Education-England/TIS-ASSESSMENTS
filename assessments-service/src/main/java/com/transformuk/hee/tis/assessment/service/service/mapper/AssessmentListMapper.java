5c1e393485f5500f6ef4c2488a15e63c550292f8
Paul Hoang <paul.hoang@hee.nhs.uk>
2017-12-08 13:39:56 +0000

    npe fix

--- assessments-service/src/main/java/com/transformuk/hee/tis/assessment/service/service/mapper/AssessmentListMapper.java
+++ assessments-service/src/main/java/com/transformuk/hee/tis/assessment/service/service/mapper/AssessmentListMapper.java
@@ -2,7 +2,6 @@ package com.transformuk.hee.tis.assessment.service.service.mapper;
 
 import com.google.common.collect.Lists;
 import com.transformuk.hee.tis.assessment.api.dto.AssessmentListDTO;
-import com.transformuk.hee.tis.assessment.api.dto.OutcomeDTO;
 import com.transformuk.hee.tis.assessment.api.dto.OutcomeStatus;
 import com.transformuk.hee.tis.assessment.service.model.Assessment;
 import com.transformuk.hee.tis.assessment.service.model.Outcome;
@@ -11,7 +10,6 @@ import org.mapstruct.InheritInverseConfiguration;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.Mappings;
-import org.mapstruct.factory.Mappers;
 
 import java.util.List;
 
@@ -37,7 +35,9 @@ public interface AssessmentListMapper extends EntityMapper<AssessmentListDTO, As
     if (CollectionUtils.isNotEmpty(outcome)) {
       outcome.sort((o1, o2) -> o2.getId().compareTo(o1.getId()));
       Outcome latestOutcome = outcome.get(0);
-      return latestOutcome.getOutcome().getLabel();
+      if (latestOutcome.getOutcome() != null) {
+        return latestOutcome.getOutcome().getLabel();
+      }
     }
     return null;
   }
