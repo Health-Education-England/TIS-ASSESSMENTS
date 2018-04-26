ecc27f8cdd111b1ecf993959b3a0ed721ec90444
Paul Hoang <paul.hoang@hee.nhs.uk>
2018-04-26 17:03:36 +0100

    bring audit columns to be in line with the columns in outcomes

---
+++ assessments-service/src/main/resources/db/migration/V1.27__Assessment_outcomes_types_out_of_sync.sql
@@ -0,0 +1,23 @@
+ALTER TABLE `AssessmentOutcome_AUD`
+MODIFY COLUMN `comments` TEXT;
+
+ALTER TABLE `AssessmentOutcome_AUD`
+MODIFY COLUMN `detailedReasons` TEXT;
+
+ALTER TABLE `AssessmentOutcome_AUD`
+MODIFY COLUMN `mitigatingCircumstances` TEXT;
+
+ALTER TABLE `AssessmentOutcome_AUD`
+MODIFY COLUMN `competencesToBeDeveloped` TEXT;
+
+ALTER TABLE `AssessmentOutcome_AUD`
+MODIFY COLUMN `otherRecommendedActions` TEXT;
+
+ALTER TABLE `AssessmentOutcome_AUD`
+MODIFY COLUMN `addCommentsFromPanel` TEXT;
+
+ALTER TABLE `AssessmentOutcome_AUD`
+MODIFY COLUMN `reason` TEXT;
+
+ALTER TABLE `AssessmentOutcome_AUD`
+MODIFY COLUMN `recommendedAddtnlTrainingTime` TEXT;
