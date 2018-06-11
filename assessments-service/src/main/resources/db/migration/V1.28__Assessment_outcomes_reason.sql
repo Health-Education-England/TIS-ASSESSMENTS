3c6eff9cc350182a2d570e96ecc2b671554f561b
Paul Hoang <paul.hoang@hee.nhs.uk>
2018-06-11 16:01:54 +0100

    missed out foreign keys

--- assessments-service/src/main/resources/db/migration/V1.28__Assessment_outcomes_reason.sql
+++ assessments-service/src/main/resources/db/migration/V1.28__Assessment_outcomes_reason.sql
@@ -3,7 +3,9 @@ CREATE TABLE `AssessmentOutcomeReason` (
   `assessmentOutcomeId` bigint(20) NOT NULL,
   `reasonId` bigint(20) NOT NULL,
   `other` TEXT DEFAULT NULL,
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`id`),
+  CONSTRAINT `fk_aor_assessment_outcome_id` FOREIGN KEY (`assessmentOutcomeId`) REFERENCES `AssessmentOutcome` (`id`),
+  CONSTRAINT `fk_aor_reason_id` FOREIGN KEY (`reasonId`) REFERENCES `Reason` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 ALTER TABLE `Reason`
