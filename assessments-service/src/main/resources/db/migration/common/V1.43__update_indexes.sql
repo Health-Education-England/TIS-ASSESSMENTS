e24b5019bca5d59c09d3f1bbcbbcf317fed81120
Reuben Roberts <reuben.roberts@hee.nhs.uk>
2021-12-09 10:00:25 +0000

    fix typo

--- assessments-service/src/main/resources/db/migration/common/V1.43__update_indexes.sql
+++ assessments-service/src/main/resources/db/migration/common/V1.43__update_indexes.sql
@@ -4,4 +4,4 @@ ALTER TABLE `Assessment` ADD INDEX `assessment_duplicates`
 (`traineeId`, `programmeMembershipId`, `reviewDate`, `softDeletedDate`);
 ALTER TABLE `Assessment` ADD INDEX `assessment_search`
 (`type`, `gmcNumber`, `gdcNumber`, `publicHealthNumber`);
-ALTEr TABLE `AssessmentDetail` ADD INDEX `assessment_detail_curriculum_name` (`curriculumName`);
+ALTER TABLE `AssessmentDetail` ADD INDEX `assessment_detail_curriculum_name` (`curriculumName`);
