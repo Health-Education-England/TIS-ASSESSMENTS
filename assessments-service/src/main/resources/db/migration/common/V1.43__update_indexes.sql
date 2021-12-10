2f9c98aa55ad91adba47d03c8dbcfb94ffb0e576
Reuben Roberts <reuben.roberts@hee.nhs.uk>
2021-12-10 16:03:55 +0000

    fix: remove search composite index
    Since the search is OR'd not AND'd, there is no value in a composite index.

--- assessments-service/src/main/resources/db/migration/common/V1.43__update_indexes.sql
+++ assessments-service/src/main/resources/db/migration/common/V1.43__update_indexes.sql
@@ -1,7 +1,9 @@
 ALTER TABLE `Assessment` ADD INDEX `assessment_soft_deleted_date` (`softDeletedDate`);
 ALTER TABLE `AssessmentOutcome` ADD INDEX `assessment_outcome_outcome` (`outcome`);
+ALTER TABLE `Assessment` DROP INDEX `assessment_trainee_id`;
 ALTER TABLE `Assessment` ADD INDEX `assessment_duplicates`
 (`traineeId`, `programmeMembershipId`, `reviewDate`, `softDeletedDate`);
-ALTER TABLE `Assessment` ADD INDEX `assessment_search`
-(`gmcNumber`, `gdcNumber`, `publicHealthNumber`);
+ALTER TABLE `Assessment` ADD INDEX `assessment_gmc_number` (`gmcNumber`);
+ALTER TABLE `Assessment` ADD INDEX `assessment_gdc_number` (`gdcNumber`);
+ALTER TABLE `Assessment` ADD INDEX `assessment_public_health_number` (`publicHealthNumber`);
 ALTER TABLE `AssessmentDetail` ADD INDEX `assessment_detail_curriculum_name` (`curriculumName`);
