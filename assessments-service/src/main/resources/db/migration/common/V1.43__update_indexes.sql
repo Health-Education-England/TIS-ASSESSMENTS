ee23e2c2ab16c97d6585a2a9ac5989225d474b5f
Reuben Roberts <reuben.roberts@hee.nhs.uk>
2021-12-10 15:26:26 +0000

    fix: rework assessment_search index

--- assessments-service/src/main/resources/db/migration/common/V1.43__update_indexes.sql
+++ assessments-service/src/main/resources/db/migration/common/V1.43__update_indexes.sql
@@ -3,5 +3,5 @@ ALTER TABLE `AssessmentOutcome` ADD INDEX `assessment_outcome_outcome` (`outcome
 ALTER TABLE `Assessment` ADD INDEX `assessment_duplicates`
 (`traineeId`, `programmeMembershipId`, `reviewDate`, `softDeletedDate`);
 ALTER TABLE `Assessment` ADD INDEX `assessment_search`
-(`type`, `gmcNumber`, `gdcNumber`, `publicHealthNumber`);
+(`gmcNumber`, `gdcNumber`, `publicHealthNumber`);
 ALTER TABLE `AssessmentDetail` ADD INDEX `assessment_detail_curriculum_name` (`curriculumName`);
