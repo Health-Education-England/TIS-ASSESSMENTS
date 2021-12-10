f06c0f326d0860cca9b2a8304f7276ce527dd12f
Reuben Roberts <reuben.roberts@hee.nhs.uk>
2021-12-10 16:22:03 +0000

    fix: dont delete traineeId index

--- assessments-service/src/main/resources/db/migration/common/V1.43__update_indexes.sql
+++ assessments-service/src/main/resources/db/migration/common/V1.43__update_indexes.sql
@@ -1,6 +1,5 @@
 ALTER TABLE `Assessment` ADD INDEX `assessment_soft_deleted_date` (`softDeletedDate`);
 ALTER TABLE `AssessmentOutcome` ADD INDEX `assessment_outcome_outcome` (`outcome`);
-ALTER TABLE `Assessment` DROP INDEX `assessment_trainee_id`;
 ALTER TABLE `Assessment` ADD INDEX `assessment_duplicates`
 (`traineeId`, `programmeMembershipId`, `reviewDate`, `softDeletedDate`);
 ALTER TABLE `Assessment` ADD INDEX `assessment_gmc_number` (`gmcNumber`);
