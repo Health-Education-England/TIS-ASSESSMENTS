960412619b3642e40cfc5bc8465de74f5f096022
Yafang Deng <hihilary@outlook.com>
2024-09-25 18:55:51 +0100

    refactor:user INSERT...SELECT...statement
    TIS21-6496

--- assessments-service/src/main/resources/db/migration/common/V1.45__add_not_assessed_outcome_reason.sql
+++ assessments-service/src/main/resources/db/migration/common/V1.45__add_not_assessed_outcome_reason.sql
@@ -1,7 +1,5 @@
 INSERT INTO `Reason` (`code`, `label`) VALUES
 ('N23', 'Assessed in another specialty (dual/triple CCT)');
 
-SET @reason_id = (SELECT id from Reason where code = 'N23');
-
-INSERT INTO OutcomeReasons (`outcomeId`, `reasonId`) VALUES
-(1, @reason_id);
+INSERT INTO `OutcomeReasons` (`outcomeId`, `reasonId`)
+SELECT 1, `id` FROM `Reason` WHERE `code` = 'N23';
