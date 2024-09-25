7284ba552c327582eaa5021e623313df9a250fb7
Yafang Deng <hihilary@outlook.com>
2024-09-25 19:16:48 +0100

    fix:use query for getting outcomeId

--- assessments-service/src/main/resources/db/migration/common/V1.45__add_not_assessed_outcome_reason.sql
+++ assessments-service/src/main/resources/db/migration/common/V1.45__add_not_assessed_outcome_reason.sql
@@ -2,4 +2,6 @@ INSERT INTO `Reason` (`code`, `label`) VALUES
 ('N23', 'Assessed in another specialty (dual/triple CCT)');
 
 INSERT INTO `OutcomeReasons` (`outcomeId`, `reasonId`)
-SELECT 1, `id` FROM `Reason` WHERE `code` = 'N23';
+SELECT o.`id`, r.`id`
+FROM `Outcome` o, `Reason` r
+WHERE o.`label` = 'Not Assessed' AND r.`code` = 'N23';
