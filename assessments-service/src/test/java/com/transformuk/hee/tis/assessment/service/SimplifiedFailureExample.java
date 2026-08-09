803f8c89d215d76fe625dde594df3176ae2d89b8
Andy Dingley <Judge40@users.noreply.github.com>
2026-08-09 02:49:51 +0100

    test: add additional failure scenario
    The tests fail when running later version of hibernate due to the
    precision on `LocalTimeDate`. Later version respect the underlying
    precision of nanoseconds nanosecond but MySQL precision caps out at
    microseconds. Furthering the issue, our timestamps are actually
    `datetime(3)` giving only milliseconds.
    
    Since `LocalDateTime` is used with `@Version` there is a very real bug
    when upgrading Spring Boot/Hibernate.
    The version is generated when a record is persisted with nanosecond
    precision, MySQL rounds the timestamp to millisecond precision.
    On the next update Hibernate's optimistic-lock check compares the full
    precision timestamp that's still in memory against whats actually in the
    DB (rounded).
    
    `UPDATE ... SET ... WHERE id = ? AND amended_date = ?`
    
    Because the precisions differ, this can never match and Hibernate
    believes the object has been changed and reports the
    ObjectOptimisticLockingFailureException.

--- assessments-service/src/test/java/com/transformuk/hee/tis/assessment/service/SimplifiedFailureExample.java
+++ assessments-service/src/test/java/com/transformuk/hee/tis/assessment/service/SimplifiedFailureExample.java
@@ -4,6 +4,8 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import com.transformuk.hee.tis.assessment.service.model.Assessment;
 import com.transformuk.hee.tis.assessment.service.repository.AssessmentRepository;
+import java.time.LocalDateTime;
+import java.util.Optional;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -32,5 +34,18 @@ public class SimplifiedFailureExample {
 
     int databaseSizeAfterUpdate = assessmentRepository.findAll().size();
     assertThat(databaseSizeAfterUpdate).isEqualTo(databaseSizeBeforeUpdate);
+  }
+
+  @Test
+  public void simplifiedFailureExampleByDate() {
+    Assessment assessment = new Assessment();
+    assessment.setFirstName("Joe");
+    assessment = assessmentRepository.saveAndFlush(assessment);
+    LocalDateTime amendedDate = assessment.getAmendedDate();
+
+    Optional<Assessment> actualStoredAssessment = assessmentRepository.findById(assessment.getId());
+    LocalDateTime actualStoredAmendedDate = actualStoredAssessment.get().getAmendedDate();
+
+    assertThat(amendedDate).isEqualTo(actualStoredAmendedDate);
   }
 }
